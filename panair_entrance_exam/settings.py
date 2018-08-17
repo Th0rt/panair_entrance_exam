9d2a1dae5aa1ca1a93d83e95f133035a74fd65ea
Th0rt <yamaxanado@gmail.com>
2018-08-17 19:22:16 +0900

    MySQL_settings

--- panair_entrance_exam/settings.py
+++ panair_entrance_exam/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'panair_entrance_exam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'panair_entrance_exam',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
