d698251dcf54310b3fde0810fdbc238f42aa8e1f
Th0rt <yamaxanado@gmail.com>
2018-08-18 00:21:33 +0900

    init user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(default=timezone.now)
