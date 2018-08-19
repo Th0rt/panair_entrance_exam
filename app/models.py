d55d821f5248729c8128c13cf7a9028ba371b0a0
Th0rt <yamaxanado@gmail.com>
2018-08-19 19:31:54 +0900

    add sex culumn in user model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    SEXES_CHOICE = ((1, '男性'), (2, '女性'))
+    name       = models.CharField(max_length=50)
+    age        = models.IntegerField()
+    sex        = models.IntegerField(choices=SEXES_CHOICE, default=1)
     created_at = models.DateTimeField(default=timezone.now)
