942f554cdb80626ed153a15b53d19fa7b03abb5f
Th0rt <yamaxanado@gmail.com>
2018-08-19 21:52:35 +0900

    Update: Create links from user/index to user/show

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('users', views.users_index, name='users')
+    path('users', views.users_index, name='users'),
+    path('users/<int:id>', views.users_show)
 ]
--- app/views.py
+++ app/views.py
@@ -9,3 +9,7 @@ def index(request):
 def users_index(request):
     users = User.objects.all()
     return render(request, 'app/users/index.html', {'users': users})
+
+def users_show(request, id):
+    user = User.objects.get(id=id)
+    return render(request, 'app/users/show.html', {'user': user})
