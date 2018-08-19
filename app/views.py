52882848da5d166bb0c386916c7ff59af84b937d
Th0rt <yamaxanado@gmail.com>
2018-08-19 22:30:05 +0900

    Update: View method

--- app/views.py
+++ app/views.py
@@ -10,6 +10,9 @@ def users_index(request):
     users = User.objects.all()
     return render(request, 'app/users/index.html', {'users': users})
 
-def users_show(request, id):
+def user_show(request, id):
     user = User.objects.get(id=id)
     return render(request, 'app/users/show.html', {'user': user})
+
+def user_new(request):
+    return render(request, 'app/users/new.html')
