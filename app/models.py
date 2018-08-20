a0d5ef0b025a2736a78414de52b691155ec07d41
Th0rt <yamaxanado@gmail.com>
2018-08-20 22:32:08 +0900

    Update: Create Curriculum Model and Lesson Model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.utils import timezone
 
+class Curriculum(models.Model):
+    name           = models.CharField(max_length=50)
+    basic_charge   = models.IntegerField()
+    metered_charge = models.IntegerField()
+    created_at     = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 class User(models.Model):
     SEXES_CHOICE = ((1, '男性'), (2, '女性'))
     name       = models.CharField(max_length=50)
     age        = models.IntegerField()
     sex        = models.IntegerField(choices=SEXES_CHOICE, default=1)
+    lessons    = models.ManyToManyField(Curriculum, through='Lesson')
+    created_at = models.DateTimeField(default=timezone.now)
+
+class Lesson(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    carriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
+    time       = models.IntegerField()
     created_at = models.DateTimeField(default=timezone.now)
