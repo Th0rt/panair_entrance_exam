c603760c3554611cceda0f72f2c4233b047f293c
Th0rt <yamaxanado@gmail.com>
2018-08-22 02:32:06 +0900

    Update: Define invoice class

--- app/models.py
+++ app/models.py
@@ -21,13 +21,6 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
-    def monthly_charge(self, year, month):
-        charge = 0
-        for lesson in self.lesson_set.filter(created_at__year  = year,
-                                             created_at__month = month):
-            charge += lesson.total_charge()
-        return charge
-
 class Lesson(models.Model):
     user       = models.ForeignKey(User, on_delete=models.CASCADE)
     curriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
@@ -39,3 +32,19 @@ class Lesson(models.Model):
 
     def total_charge(self):
         return self.curriculum.basic_charge + self.metered_charge()
+
+class Invoice:
+
+    def __init__(self, user, year, month):
+        self.user = user
+        self.lessons = user.lesson_set.filter(created_at__year = year, created_at__month = month)
+        self.charge = sum([lesson.total_charge() for lesson in self.lessons])
+
+    @property
+    def curriculum_list(self):
+        curriulum_list = []
+        for lesson in self.lessons:
+            if lesson.curriculum.name not in curriulum_list:
+                curriulum_list.append(lesson.curriculum.name)
+        return curriulum_list
+
